dd9c16d9458a5208e3e5f115fb2ea529c5a75cdb
taurusilver7 <prasad20697@gmail.com>
2022-05-18 20:51:38 +0530

    state applied on Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
-const Cart = () => {
+const Cart = ({ onClose }) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 249 }].map((item) => (
@@ -11,14 +11,16 @@ const Cart = () => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>299</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"]}>Close</button>
+        <button onClick={onClose} className={classes["button-alt"]}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
