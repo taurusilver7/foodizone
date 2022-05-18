16ccd0eba55a5021a131e672bd37b7b5bfdda4ca
taurusilver7 <prasad20697@gmail.com>
2022-05-18 17:38:58 +0530

    meal form input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = ({ label, input }) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={input.id}>{label}</label>
+      <input id={input.id} {...input} />
+    </div>
+  );
+};
+
+export default Input;
