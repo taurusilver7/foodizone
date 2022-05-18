12c3a1169e05fb7239081e1e146331b27b636162
taurusilver7 <prasad20697@gmail.com>
2022-05-18 23:07:25 +0530

    cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,42 @@
 // To manage the cartContext data and provide the data access to all required components.
 
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./CartContext";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 // The cardcontext.provider wraps around the components in need of the props data. can also add the logic for managing the context data to
 
 const CartProvider = (props) => {
-  const addItemHandler = (item) => {};
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
 
-  const removeItemHandler = (id) => {};
+  const addItemHandler = (item) => {
+    dispatchCart({ type: "ADD", item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCart({ type: "REMOVE", id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
