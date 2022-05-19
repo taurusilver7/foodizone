d3f0cbf94716336527b09f6d5d8e023913e93787
taurusilver7 <prasad20697@gmail.com>
2022-05-19 23:34:38 +0530

    meal summary updated

--- src/components/Meals/MealSummary.js
+++ src/components/Meals/MealSummary.js
@@ -6,8 +6,8 @@ const MealSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food, at your doorstep</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your favorite meal from our broad selection of meals and enjoy a
+        delicious lunch or dinner at home.
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
