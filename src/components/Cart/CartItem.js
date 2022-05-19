7b61aa35d4d8f0b793acfbce27b49d0227161497
taurusilver7 <prasad20697@gmail.com>
2022-05-19 13:50:10 +0530

    custom cart components

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,23 @@
+import classes from "./CartItem.module.css";
+
+const CartItem = ({ price, name, amount, onRemove, onAdd }) => {
+  const updatedPrice = `₹${price.toFixed(2)}`;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <div>
+        <h2>{name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{updatedPrice}</span>
+          <span className={classes.amount}>x {amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={onRemove}>−</button>
+        <button onClick={onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
