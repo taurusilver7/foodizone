3078ec1de5908c6410cee1f8022872d7c1913036
taurusilver7 <prasad20697@gmail.com>
2022-05-19 13:51:19 +0530

    dummy data updated

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,25 +8,25 @@ const DUMMY_MEALS = [
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
-    price: 22.99,
+    price: 549.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
-    price: 16.5,
+    price: 299.99,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
-    price: 12.99,
+    price: 169.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
     description: "Healthy...and green...",
-    price: 18.99,
+    price: 89.99,
   },
 ];
 
