807632305ffed4a1fa5dac6e943ec9281de5e0c6
taurusilver7 <prasad20697@gmail.com>
2022-05-19 13:53:16 +0530

    Input component forwardRef form input

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-const Input = ({ label, input }) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={input.id}>{label}</label>
-      <input id={input.id} {...input} />
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} id={props.input.id} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
