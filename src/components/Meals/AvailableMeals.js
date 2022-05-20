36c69f3d74826410a0fa5b44329305494783b12e
taurusilver7 <prasad20697@gmail.com>
2022-05-20 13:30:53 +0530

    https req init

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 549.99,
-    img: "",
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 299.99,
-    img: "",
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 169.99,
-    img: "",
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 89.99,
-    img: "",
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  // https fetch req
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://foodizone-d0523-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      const resData = await response.json(); // The resData obtained is an object with m1-m4 keys and the data in nested objects. To turn it into an array for mapping...
+
+      console.log(resData);
+      const loadedMeals = [];
+
+      for (let key in resData) {
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price,
+          img: resData[key].img,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
